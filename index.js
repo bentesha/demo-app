159f964f5a88240070cff6f4e8840d08225245ee
bentesha <benedict.tesha@jamaatech.com>
2019-09-26 18:39:23 +0300

    Create api for creating customers

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const morgan = require('morgan')
+const Joi = require('joi')
+const shortid = require('shortid')
 
 const app = express()
 app.use(morgan('tiny'))
+app.use(express.json())
 
 const router = express.Router()
+const customers = []
+
 
 router.get('/', (request, response, next) => {
   response.send('Hello There!')
 })
 
+router.post('/customers/', ({ body }, response, next) => {
+  (async () => {
+    const schema = Joi.object({
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required()
+    })
+
+    const { error, value } = schema.validate(body)
+    if (error) { return next(error) }
+    value.id = shortid.generate()
+    customers.push(value)
+    response.json(value)
+  })().catch(next)
+})
+
 app.use(router)
+app.use((error, request, response, next) => {
+  if (error.isJoi) {
+    return response.send('Sorry, you have validation error!')
+  }
+  next(error)
+})
 
 app.listen(8080, () => console.log('Listening on port:', 8080))
