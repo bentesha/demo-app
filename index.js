9afeff199bcf116e6f26bd976075bfca9d591d61
bentesha <benedict.tesha@jamaatech.com>
2019-09-26 18:44:25 +0300

    Move routes to separate file

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
-const Joi = require('joi')
-const shortid = require('shortid')
 
 const app = express()
 app.use(morgan('tiny'))
 app.use(express.json())
 
 const router = express.Router()
-const customers = []
 
+const routes = require('./routes')
 
-router.get('/', (request, response, next) => {
-  response.send('Hello There!')
-})
-
-router.post('/customers/', ({ body }, response, next) => {
-  (async () => {
-    const schema = Joi.object({
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required()
-    })
-
-    const { error, value } = schema.validate(body)
-    if (error) { return next(error) }
-    value.id = shortid.generate()
-    customers.push(value)
-    response.json(value)
-  })().catch(next)
-})
-
-app.use(router)
+app.use(routes)
 app.use((error, request, response, next) => {
   if (error.isJoi) {
     return response.send('Sorry, you have validation error!')
---
+++ routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const Joi = require('joi')
+const shortid = require('shortid')
+
+const customers = []
+
+const router = express.Router()
+
+router.get('/', (request, response, next) => {
+  response.send('Hello There!')
+})
+
+router.post('/customers/', ({ body }, response, next) => {
+  (async () => {
+    const schema = Joi.object({
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required()
+    })
+
+    const { error, value } = schema.validate(body)
+    if (error) { return next(error) }
+    value.id = shortid.generate()
+    customers.push(value)
+    response.json(value)
+  })().catch(next)
+})
+
+module.exports = router
